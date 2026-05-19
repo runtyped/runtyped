9882b2e1888b073fec972ce374862a249155e577
Jacopo Scazzosi <jacopo@scazzosi.com>
2026-05-19 12:15:53 +0200

    chore(type): adds a few more basic tests for toJsonSchema()

--- packages/type/tests/jsonschema.spec.ts
+++ packages/type/tests/jsonschema.spec.ts
@@ -5,9 +5,40 @@ import { toJsonSchema } from '../src/jsonschema/jsonschema';
 describe('toJsonSchema', () => {
 
   test('should handle numbers', () => {
-    type numeric = number;
-    const schema = toJsonSchema<numeric>();
+    type Numeric = number;
+    const schema = toJsonSchema<Numeric>();
     expect(schema).toEqual({ type: 'number' });
+  });
+
+  test('should handle booleans', () => {
+    type Booleany = boolean;
+    const schema = toJsonSchema<Booleany>();
+    expect(schema).toEqual({ type: 'boolean' });
+  });
+
+  test('should handle objects', () => {
+    type Objecty = { a: 5 };
+    const schema = toJsonSchema<Objecty>();
+    expect(schema).toEqual({
+      type: 'object',
+      properties: {
+        a: { type: 'number' },
+      },
+      required: ['a'],
+    });
+  });
+
+  test('should handle objects with optional properties', () => {
+    type ObjectyOptProps = { a?: 5, b: 7 };
+    const schema = toJsonSchema<ObjectyOptProps>();
+    expect(schema).toEqual({
+      type: 'object',
+      properties: {
+        a: { type: 'number' },
+        b: { type: 'number' },
+      },
+      required: ['b'],
+    });
   });
 
 
