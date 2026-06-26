fe1f5a363daa79547f7f51d15d1dda915aae62bb
Sage <sage@runtyped.dev>
2026-06-26 20:08:43 +0200

    chore(bench): benchmarks for serialize(), deserialize() and serialization rountrip

--- packages/bench/index.ts
+++ packages/bench/index.ts
@@ -2,5 +2,8 @@ import { run } from 'mitata';
 
 import './src/is.js';
 import './src/cast.js';
+import './src/serialize.js';
+import './src/deserialize.js';
+import './src/roundtrip.js';
 
 await run();
